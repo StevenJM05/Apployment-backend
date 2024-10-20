c22a4c198049ff4a113f398038521c4356367482
StevenJM05 <116135869+StevenJM05@users.noreply.github.com>
2024-10-19 21:11:27 -0600

    Crear la migración [ara la tabla profile

---
+++ database/migrations/2024_10_20_030559_create_profile_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('names');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('photo');
+            $table->float('qualifications');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile');
+    }
+};
